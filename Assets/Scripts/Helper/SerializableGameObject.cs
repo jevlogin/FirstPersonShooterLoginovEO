8a440eb04e207523427a7091b3254b7ef390f788
jevlogin <jev.login@mail.ru>
2020-05-20 17:45:55 +0300

    Создана структура SerializableVector3. Заполнен класс TestAttribute material создает новый экземпляр класса материал, а sharedMaterial обращается к источнику материала, и его изменяет

---
+++ Assets/Scripts/Helper/SerializableGameObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace JevLogin
+{
+    [Serializable]
+    public struct SerializableGameObject
+    {
+        public string Name;
+        public SerializableVector3 Position;
+        public SerializableQuaternion Rotation;
+        public SerializableVector3 Scale;
+        public Component[] Components;
+    }
+
+    
+
+    [Serializable]
+    public struct SerializableVector3
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public SerializableVector3(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public static implicit operator Vector3(SerializableVector3 value)
+        {
+            return new Vector3(value.X, value.Y, value.Z);
+        }
+        public static implicit operator SerializableVector3(Vector3 value)
+        {
+            return new SerializableVector3(value.X, value.Y, value.Z);
+        }
+
+    }
+
+    [Serializable]
+    public struct SerializableQuaternion
+    {
+
+    }
+}
---
+++ Assets/Scripts/Test/TestAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    [RequireComponent(typeof(Renderer)), ExecuteInEditMode, DisallowMultipleComponent]
+    public class TestAttribute : MonoBehaviour
+    {
+        [HideInInspector] public float TestHidePublic;
+        [SerializeField] private float _testSerializePrivate = 7.0f;
+        private float _testPrivateTwo;
+        public SerializableGameObject SerializableGameObject;
+
+        private const int _min = 0;
+        private const int _max = 100;
+        [Header("Test variables")]
+        [ContextMenuItem("Randomize Number", nameof(Randomize))]
+        [Range(_min, _max)]
+        public int SecondTest;
+
+        
+
+        [Space(60)]
+        [SerializeField, Multiline(5)] private string _testMultiline;
+        [Space(60)]
+        [SerializeField, TextArea(5, 5), Tooltip("Tooltip text")] private string _testArea;
+
+        private void Update()
+        {
+            GetComponent<Renderer>().sharedMaterial.color = UnityEngine.Random.ColorHSV();
+        }
+
+        private void Randomize()
+        {
+            SecondTest = UnityEngine.Random.Range(_min, _max);
+        }
+
+        [Obsolete("Устарело. Используй что-то другое")]
+        private void TestObsolete()
+        {
+
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Button("Click me");
+        }
+    }
+}
