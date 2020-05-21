c3ff0d6e3360fc57dc4c892090a4486d5d8e3f2f
jevlogin <jev.login@mail.ru>
2020-05-21 20:35:51 +0300

    Дописали структуру SerializableGameObject

--- Assets/Scripts/Helper/SerializableGameObject.cs
+++ Assets/Scripts/Helper/SerializableGameObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Numerics;
+using UnityEditor;
 
 namespace JevLogin
 {
@@ -14,7 +15,7 @@ namespace JevLogin
         public Component[] Components;
     }
 
-    
+
 
     [Serializable]
     public struct SerializableVector3
@@ -42,6 +43,25 @@ namespace JevLogin
     [Serializable]
     public struct SerializableQuaternion
     {
+        public float X;
+        public float Y;
+        public float Z;
+        public float W;
 
+        public SerializableQuaternion(float x, float y, float z, float w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+        public static implicit operator Quaternion(SerializableQuaternion value)
+        {
+            return new Quaternion(value.X, value.Y, value.Z, value.W);
+        }
+        public static implicit operator SerializableQuaternion(Quaternion value)
+        {
+            return new SerializableQuaternion(value.X, value.Y, value.Z, value.W);
+        }
     }
 }
